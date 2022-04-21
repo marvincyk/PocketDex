52554c8e213c34d4a9dee433ee73c367cf83796b
Marvin Chew <marvin@marvincyk.com>
2022-04-22 00:14:04 +1000

    Implement search functionality

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { SafeAreaView, View } from "react-native";
 import tw from "twrnc";
 import Card from "./components/Card";
@@ -6,15 +7,34 @@ import SearchBar from "./components/SearchBar";
 import data from "./data.json";
 
 export default function App() {
+  const [search, setSearch] = useState("");
+
+  const handleChangeText = (value: string) => {
+    setSearch(value);
+  };
+
   return (
     <SafeAreaView
       style={tw`flex bg-white items-center justify-center h-full w-full`}
     >
-      <SearchBar />
+      <SearchBar
+        onChangeText={handleChangeText}
+        placeholder="Search for a Pokémon"
+        search={search}
+      />
       <View style={tw`flex flex-row flex-wrap w-4/5 justify-around mt-10`}>
-        {data.pokemon.map((pokemon) => (
-          <Card image={pokemon.image} name={pokemon.name} key={pokemon.id} />
-        ))}
+        {data.pokemon
+          .filter((pokemon) => {
+            if (
+              search == "" ||
+              pokemon.name.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return pokemon;
+            }
+          })
+          .map((pokemon) => (
+            <Card image={pokemon.image} name={pokemon.name} key={pokemon.id} />
+          ))}
       </View>
       <StatusBar style="auto" />
     </SafeAreaView>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import { TextInput } from "react-native";
 import tw from "twrnc";
 
-function SearchBar() {
-  const [search, setSearch] = useState("");
+interface SearchBarProps {
+  onChangeText: (value: string) => void;
+  search: string;
+  placeholder?: string;
+}
 
+function SearchBar({ onChangeText, search, placeholder }: SearchBarProps) {
   return (
     <TextInput
       style={tw`w-4/5 border rounded-full p-4`}
       clearButtonMode="unless-editing"
       returnKeyType="search"
-      onChangeText={(value) => setSearch(value)}
-      placeholder="Search for a Pokémon"
+      onChangeText={onChangeText}
+      placeholder={placeholder}
       value={search}
     />
   );
