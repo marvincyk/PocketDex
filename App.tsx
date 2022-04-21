756971ca8a1bda42b4907de802ed8856518033f1
Marvin Chew <marvin@marvincyk.com>
2022-04-22 00:32:18 +1000

    Implement tap outside to dismiss keyboard

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { SafeAreaView, View } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 import tw from "twrnc";
 import Card from "./components/Card";
 import SearchBar from "./components/SearchBar";
@@ -14,29 +19,35 @@ export default function App() {
   };
 
   return (
-    <SafeAreaView
-      style={tw`flex bg-white items-center justify-center h-full w-full`}
-    >
-      <SearchBar
-        onChangeText={handleChangeText}
-        placeholder="Search for a Pokémon"
-        search={search}
-      />
-      <View style={tw`flex flex-row flex-wrap w-4/5 justify-around mt-10`}>
-        {data.pokemon
-          .filter((pokemon) => {
-            if (
-              search == "" ||
-              pokemon.name.toLowerCase().includes(search.toLowerCase())
-            ) {
-              return pokemon;
-            }
-          })
-          .map((pokemon) => (
-            <Card image={pokemon.image} name={pokemon.name} key={pokemon.id} />
-          ))}
-      </View>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView
+        style={tw`flex bg-white items-center justify-center h-full w-full`}
+      >
+        <SearchBar
+          onChangeText={handleChangeText}
+          placeholder="Search for a Pokémon"
+          search={search}
+        />
+        <View style={tw`flex flex-row flex-wrap w-4/5 justify-around mt-10`}>
+          {data.pokemon
+            .filter((pokemon) => {
+              if (
+                search == "" ||
+                pokemon.name.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return pokemon;
+              }
+            })
+            .map((pokemon) => (
+              <Card
+                image={pokemon.image}
+                name={pokemon.name}
+                key={pokemon.id}
+              />
+            ))}
+        </View>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
